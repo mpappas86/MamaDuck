108f5eba39ab515fc5991cde502a59fe233b9048
cookesan <thcprime@gmail.com>
2017-02-20 20:13:33 -0500

    SFX handler update

--- Assets/Scripts/SfxHandler.cs
+++ Assets/Scripts/SfxHandler.cs
@@ -23,7 +23,7 @@ public class SfxHandler : MonoBehaviour {
         // load sound effects with resources.load once in Awake
         gameObject.AddComponent<AudioSource>();
 
-        clipHolder = new AudioClip[7];
+        clipHolder = new AudioClip[9];
         clipHolder[0] = Resources.Load("SoundFX/pause") as AudioClip;
         clipHolder[1] = Resources.Load("SoundFX/ascending_tier") as AudioClip;
         clipHolder[2] = Resources.Load("SoundFX/descending_tier") as AudioClip;
@@ -31,6 +31,9 @@ public class SfxHandler : MonoBehaviour {
         clipHolder[4] = Resources.Load("SoundFX/duckling_death") as AudioClip;
         clipHolder[5] = Resources.Load("SoundFX/duck_zapped") as AudioClip;
         clipHolder[6] = Resources.Load("SoundFX/level_completed") as AudioClip;
+        clipHolder[7] = Resources.Load("SoundFX/current_water") as AudioClip;
+        clipHolder[8] = Resources.Load("SoundFX/geyser_effect") as AudioClip;
+
     }
 
     // Use this for initialization
